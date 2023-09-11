e2242023b60a22f0a104c0411b2398bae41f298b
VAVASIN <vasin.nksoft@gmail.com>
2023-09-11 23:36:25 +0400

    add: 05-virt-04-docker-compose

---
+++ 05-virt-04-docker-compose/src/terraform/variables.tf
@@ -0,0 +1,17 @@
+# Заменить на ID своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_cloud_id" {
+  default = "b1ge3dhrtnit6a2slcne"
+}
+
+# Заменить на Folder своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_folder_id" {
+  default = "b1ge3dhrtnit6a2slcne"
+}
+
+# Заменить на ID своего образа
+# ID можно узнать с помощью команды yc compute image list
+variable "centos-7-base" {
+  default = "fd8and2naljf8m1b7brb"
+}
